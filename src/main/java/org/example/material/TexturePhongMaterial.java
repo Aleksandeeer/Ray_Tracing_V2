f388454751c6470953977b853394509fbdfcc386
Aleksandr Kuznetsov <kuznecovaleksandr923@gmail.com>
2025-06-24 22:54:32 +0300

    Texture phong material added

---
+++ src/main/java/org/example/material/TexturePhongMaterial.java
@@ -0,0 +1,52 @@
+package org.example.material;
+
+import org.example.material.textures.Texture;
+import org.example.math.Ray;
+import org.example.objects.HitResult;
+
+import java.awt.*;
+
+public class TexturePhongMaterial implements Material {
+    private final Texture texture;
+    private final Color ambient;
+    private final Color specular;
+    private final double shininess;
+
+    public TexturePhongMaterial(Texture texture, Color ambient, Color specular, double shininess) {
+        this.texture = texture;
+        this.ambient = ambient;
+        this.specular = specular;
+        this.shininess = shininess;
+    }
+
+    @Override
+    public boolean scatter(Ray rayIn, HitResult hit, ScatterResult result) {
+        return false; // не рассеивает свет — используется фонг-освещение
+    }
+
+    @Override
+    public Color getColor() {
+        return Color.WHITE;
+    }
+
+    @Override
+    public Color getAmbient(HitResult hit) {
+        return ambient;
+    }
+
+    @Override
+    public Color getDiffuse(HitResult hit) {
+        return texture.getColor(hit.u, hit.v, hit.point);
+    }
+
+    @Override
+    public Color getSpecular(HitResult hit) {
+        return specular;
+    }
+
+    @Override
+    public double getShininess() {
+        return shininess;
+    }
+}
+
